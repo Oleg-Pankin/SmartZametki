f4ea664f3207414beb80bb3263b98db08b56fa99
Олег <alexpankin1972@gmail.com>
2021-12-12 14:23:12 +0500

    pochti robit

--- main.py
+++ main.py
@@ -9,23 +9,26 @@ class mywindow(QtWidgets.QMainWindow):
         super(mywindow, self).__init__()
         self.ui = ui_main.Ui_MainWindow()
         self.ui.setupUi(self)
-        self.note.clicked.connect(self.show_note)
-        self.create_note.clicked.connect(self.add_note)
-        self.delete_note.clicked.connect(self.del_note)
-        self.save_note.clicked.connect(self.save_note)
+        self.list_notes()
+        self.ui.notes.itemClicked.connect(self.show_note)
+        self.ui.create_note.clicked.connect(self.add_note)
+        self.ui.delete_note.clicked.connect(self.del_note)
+        #self.ui.save_note.clicked.connect(self.save_note)
     def add_note(self):
         with open('note.json', 'a') as file:
             data = json.dump(file)
     def show_note(self):
         with open('note.json', 'r') as file:
             data = json.load(file)
+            self.ui.note.setPlainText(data["Text"])
     def del_note(self):
         with open('note.json', 'a') as file:
             data = json.dump(file)
     def list_notes(self):
         with open('note.json', 'r') as file:
             data = json.load(file)
-            self.notes.addItem(data)
+            print(data)
+            self.ui.notes.addItem(data["Name"])
 if __name__ == '__main__':
     app = QtWidgets.QApplication([])
     window = mywindow()
